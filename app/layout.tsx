55688d9064b16574b73ce859e1175b0556a52d91
Shinas Muhammed <shinasmuhd43@gmail.com>
2026-03-11 17:24:01 +0530

    font problem commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,13 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Anton } from "next/font/google";
 import './globals.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const anton = Anton({
   variable: "--font-anton",
   subsets: ["latin"],
   weight: "400", // Anton only has one weight
-  display: "swap",
+  
 });
 
 export const metadata: Metadata = {
@@ -32,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${anton.variable} antialiased`}
+        className={`${anton.variable} antialiased`}
       >
         {children}
       </body>
